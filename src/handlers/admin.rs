c6a4149c62a99d6c6f4658faf5fcc4cfc0fc8d85
河野 裕隆 <hirotaka.kouno@toranoana.co.jp>
2021-10-25 12:32:52 +0900

    不要なインポート削除 末尾の改行追加

--- src/handlers/admin.rs
+++ src/handlers/admin.rs
@@ -1,17 +1,11 @@
 use crate::db::manager::DataPgPool;
-use crate::graphql::schema::Schema;
-use actix_web::{error, middleware, web, App, Error, HttpResponse, HttpServer, Result};
-use juniper::http::GraphQLRequest;
-use log::debug;
-use std::sync::Arc;
-use tera::Tera;
+use actix_web::{error, web, Error, HttpResponse, Result};
 
 pub mod project;
 
-
-pub async fn index(tmpl: web::Data<tera::Tera>, pool: DataPgPool) -> Result<HttpResponse, Error> {
+pub async fn index(tmpl: web::Data<tera::Tera>, _: DataPgPool) -> Result<HttpResponse, Error> {
     let mut ctx = tera::Context::new();
-    ctx.insert("name", "kouno");
+    ctx.insert("name", "hallo world");
     Ok(HttpResponse::Ok().content_type("text/html").body(tmpl.render("index.html", &ctx)
       .map_err(|_| error::ErrorInternalServerError("Template error"))?))
 }
